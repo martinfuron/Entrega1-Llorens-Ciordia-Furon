1e525ffbd5f25fda5e3c5caa102a238b884b7b5f
Vickyllorens <vickyllorens14@gmail.com>
2022-10-10 14:41:14 -0300

    agrego formulario para autores

--- ProyectoFinal/Blog/views.py
+++ ProyectoFinal/Blog/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Blog.models import Autor, Articulo, Section
 
 # Create your views here.
 
 def mostrar_inicio(request):    
-    return render(request,"Blog/inicio.html")
+    return render(request,"blog/inicio.html")
 
 def mostrar_autores(request):
-    return render(request,"Blog/autor.html")
+    return render(request,"blog/autor.html")
 
 
 def mostrar_articulos(request):
-    return render(request,"Blog/articulo.html")
+    return render(request,"blog/articulo.html")
 
 
 def mostrar_seccion(request):
-    return render(request,"Blog/seccion.html")
+    return render(request,"blog/seccion.html")
+
+
+def procesar_formulario(request):
+    if request.method !="POST":
+        return render(request, "blog/formularioautor.html")
+    
+    autor=Autor ( nombre=request.POST["nombre"],apellido=request.POST["apellido"],profesion=request.POST["profesion"])
+    autor.save()
+    return render(request,"blog/inicio.html")
