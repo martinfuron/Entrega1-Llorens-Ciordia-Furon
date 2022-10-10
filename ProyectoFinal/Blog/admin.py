7ba3f31db9a59a336806720d18a6c4601f78529d
Vickyllorens <vickyllorens14@gmail.com>
2022-10-10 11:50:18 -0300

    modelos

--- ProyectoFinal/Blog/admin.py
+++ ProyectoFinal/Blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Blog.models import Articulo, Autor , Section
 
 # Register your models here.
+
+admin.site.register(Articulo)
+admin.site.register(Autor)
+admin.site.register(Section)
--- ProyectoFinal/Blog/models.py
+++ ProyectoFinal/Blog/models.py
@@ -3,16 +3,31 @@ from django.db import models
 # Create your models here.
 
 class Autor (models.Model):
+    
+    class Meta:
+        verbose_name_plural ="Autores"
+    
     nombre=models.CharField(max_length=40)
     apellido=models.CharField(max_length=40)
     profesion=models.CharField(max_length=40)
+    
+    def __str__(self):
+        return (f"{self.nombre} {self.apellido}")
 
 
 class Articulo (models.Model):
+    
+    class Meta:
+        verbose_name_plural ="Articulos"
+    
     titulo=models.CharField(max_length=30)
     texto=models.CharField(max_length=1500)
     fecha=models.DateField(null=True)
 
 
-class Secton (models.Model):
+class Section (models.Model):
+    
+    class Meta:
+        verbose_name_plural ="Secciones"
+    
     nombre=models.CharField(max_length=40)
