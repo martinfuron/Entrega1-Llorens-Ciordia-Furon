ce946351adfbe65d51c347b9b0008ccca13ad855
Vickyllorens <vickyllorens14@gmail.com>
2022-10-10 14:42:14 -0300

    formulario

--- ProyectoFinal/Blog/urls.py
+++ ProyectoFinal/Blog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from Blog.views import mostrar_inicio, mostrar_articulos, mostrar_autores, mostrar_seccion
+from Blog.views import mostrar_inicio, mostrar_articulos, mostrar_autores, mostrar_seccion, procesar_formulario
 
 urlpatterns = [
     path("inicio/",mostrar_inicio),
     path("autor/",mostrar_autores),
     path("articulo/",mostrar_articulos),
-    path("seccion/",mostrar_seccion)
+    path("seccion/",mostrar_seccion),
+    path ("formularioautor/", procesar_formulario, name="formularioautor")
 ]
